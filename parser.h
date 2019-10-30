4c803928b05c8ab2235ea37c001d8dcef10f2ee1
xandrl09 <47797329+xandrl09@users.noreply.github.com>
2019-10-30 18:39:26 +0100

    Add complete parser.h

---
+++ parser.h
@@ -0,0 +1,47 @@
+/**
+ * Subject: IFJ & IAL
+ *
+ * Project: Implementation of project IFJ18.
+ *
+ * @brief Parser.
+ *
+ * @author Ondřej Andrla <xandrl09@stud.fit.vutbr.cz>
+ */
+
+#pragma once
+
+#include "scanner.h"
+#include "dynid.h"
+#include "error.h"
+#include "symtable.h"
+//#include "func.h"
+// "expresions.h"
+
+// hlavicka pro syntakticky analyzator
+
+
+/**
+ * @struct Parser's internal data representation.
+ */
+typedef struct
+{
+
+    STmaintablePtr table; /// CHECK
+
+    T_token *token;
+    T_token *second_token;
+    T_token *third_token;
+
+    T_token *func_id;
+
+    dyn_id* str;
+
+    bool in_function;			/// Defines if the parser is in function
+
+    bool non_declared_function;	/// Function that has been only defined
+
+    STsymPtr symb; /// sym
+} MainData;
+
+
+int parse();
