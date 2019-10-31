e27e4c1350256b435a3a670b95db7641f5e88a14
xandrl09 <47797329+xandrl09@users.noreply.github.com>
2019-10-31 19:11:22 +0100

    Add main.h
    Contains enums for expressions analysis

---
+++ main.h
@@ -0,0 +1,34 @@
+/**
+ * Author: Ondřej Andrla
+ * datum: 2019
+ */
+
+#pragma once
+
+/**
+ * @enum Symbols used for precednece analysis.
+ */
+typedef enum symbol_enum
+{
+    PLUS,			/// +
+    MINUS,			/// -
+    MUL_,			/// *
+    DIV_,			/// /
+    EQ_,				/// =
+    NEQ,			/// <>
+    LEQ,			/// <=
+    LESS,			/// <
+    MEQ,			/// >=
+    MORE,			/// >
+    LEFT_BRACKET_,	/// (
+    RIGHT_BRACKET_,	/// )
+    ID,		/// ID
+    INT_,		/// int
+    FLOAT_,	/// double
+    STRING_,			/// string
+    DOLLAR,			/// $
+    STOP,			/// stop symbol used when reducing
+    NON_TERM		/// non-terminal
+} symbol_enum;
+
+
